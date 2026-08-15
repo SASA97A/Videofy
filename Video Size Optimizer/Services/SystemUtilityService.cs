9ec47f2aa73b2fed93d992299f6476657ed16004
SASA97A <safarahmad@hotmail.com>
2026-08-15 20:35:56 +0200

    fix: set System.AppUserModel.ID property on Videofy shortcut so Windows Action Center displays Videofy app header

--- Video Size Optimizer/Services/SystemUtilityService.cs
+++ Video Size Optimizer/Services/SystemUtilityService.cs
@@ -300,13 +300,86 @@ namespace Video_Size_Optimizer
 
                         string psScript = $@"
 $shortcutPath = ""$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Videofy.lnk""
+if (-not (Test-Path $shortcutPath)) {{
+    try {{
+        $shell = New-Object -ComObject WScript.Shell
+        $shortcut = $shell.CreateShortcut($shortcutPath)
+        $shortcut.TargetPath = '{currentExe}'
+        $shortcut.WorkingDirectory = '{currentDir}'
+        $shortcut.Description = 'Videofy Video Size Optimizer'
+        $shortcut.Save()
+    }} catch {{ }}
+}}
+
+$code = @'
+using System;
+using System.Runtime.InteropServices;
+
+public class ShellPropertySetter
+{{
+    [Guid(""886D8EEB-8CF2-4446-8D02-CDA11DC9F099""), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IPropertyStore
+    {{
+        uint GetCount();
+        void GetAt(uint iProp, out PROPERTYKEY pkey);
+        void GetValue(ref PROPERTYKEY key, out PROPVARIANT pv);
+        void SetValue(ref PROPERTYKEY key, ref PROPVARIANT pv);
+        void Commit();
+    }}
+
+    [StructLayout(LayoutKind.Sequential, Pack = 4)]
+    public struct PROPERTYKEY
+    {{
+        public Guid fmtid;
+        public uint pid;
+    }}
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct PROPVARIANT
+    {{
+        [FieldOffset(0)] public ushort vt;
+        [FieldOffset(8)] public IntPtr pwszVal;
+    }}
+
+    [DllImport(""shell32.dll"", CharSet = CharSet.Unicode, SetLastError = true)]
+    public static extern int SHGetPropertyStoreFromParsingName(
+        string pszPath,
+        IntPtr pbc,
+        uint flags,
+        ref Guid riid,
+        out IPropertyStore ppv);
+
+    public static void SetAppId(string shortcutPath, string appId)
+    {{
+        try {{
+            Guid iid = new Guid(""886D8EEB-8CF2-4446-8D02-CDA11DC9F099"");
+            IPropertyStore store;
+            int hr = SHGetPropertyStoreFromParsingName(shortcutPath, IntPtr.Zero, 2, ref iid, out store);
+            if (hr == 0 && store != null)
+            {{
+                PROPERTYKEY key = new PROPERTYKEY
+                {{
+                    fmtid = new Guid(""9F4C2855-9F79-4B39-A8D0-E1D42DE1D5F3""),
+                    pid = 5
+                }};
+                PROPVARIANT pv = new PROPVARIANT
+                {{
+                    vt = 31,
+                    pwszVal = Marshal.StringToCoTaskMemUni(appId)
+                }};
+                store.SetValue(ref key, ref pv);
+                store.Commit();
+            }}
+        }} catch {{ }}
+    }}
+}}
+'@
+
 try {{
-    $shell = New-Object -ComObject WScript.Shell
-    $shortcut = $shell.CreateShortcut($shortcutPath)
-    $shortcut.TargetPath = '{currentExe}'
-    $shortcut.WorkingDirectory = '{currentDir}'
-    $shortcut.Description = 'Videofy Video Size Optimizer'
-    $shortcut.Save()
+    if (-not ([System.Management.Automation.PSTypeName]'ShellPropertySetter').Type) {{
+        Add-Type -TypeDefinition $code
+    }}
+    [ShellPropertySetter]::SetAppId($shortcutPath, ""Videofy"")
 }} catch {{ }}
 
 try {{
